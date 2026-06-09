b410244e5cd2934c59b1137cb174042fe1c0bbc7
Arne ten Ham <arne.tenham@vodafoneziggo.com>
2026-06-09 10:56:15 +0200

    change default path

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,9 @@ const config = {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
+		paths: {
+			base: '/reeminders'
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
